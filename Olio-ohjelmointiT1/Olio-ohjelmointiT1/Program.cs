18f415979d3ba8ab8b70c94990a555936b8e94ef
Nipde <nipde@hotmail.fi>
2017-01-17 16:57:35 +0200

    prkl

---
+++ Olio-ohjelmointiT1/Olio-ohjelmointiT1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Viikkotehtavat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tehtava1();
+            //Tehtava2();
+        }
+        static void Tehtava1()
+        {
+            Console.WriteLine("Anna luku 1,2 tai 3 ja tulostan sen tekstinä");
+            string retval = Console.ReadLine();
+            int luku = int.Parse(retval);
+            if (luku == 1)
+            {
+                retval = "yksi";
+            }
+            else
+            {
+
+            }
+            Console.WriteLine(retval);
+            Console.ReadLine();
+        }
+    }
+}
